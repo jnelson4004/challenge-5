b033779647db94013b5049523e4922733c41a798
burt reynolds <jnelson4004@gmail.com>
2024-01-31 19:50:55 -0600

    post class commit

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+
+
+$(document).ready(function () {
+  var today = dayjs();
+  $("#date").text(today.format("ddd MMM DD YYYY"));
+  console.log(today);
+
+  var saveBtn = document.querySelectorAll("#saveBtn");
+
+  /* $(saveBtn).addEventListener("click", function () {
+
+  }); */
+
+  var x = dayjs().format("HH");
+  
+  function colorCode () {
+    if (x = div.property(class{})) {
+      $( "#saveBtn" ).on( "click", function() {
+        $( "#effect" ).addClass( "newClass", 1000, callback );
+      });
+  };
+  
+  
+  
+});
+
+
+
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +49,5 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
+
